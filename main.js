21ea03b44e3e6e2d14728f4a12d2b9ecfa2d09c9
SharonNabongo <sharonnabongo@gmail.com>
2022-10-21 07:42:10 -0700

    travel-images

---
+++ main.js
@@ -0,0 +1,18 @@
+const items = document.querySelectorAll(".panel");
+items.forEach(function(item) {
+    item.addEventListener('click', function(){
+        removeActiveClasses();
+        item.classList.add("active");
+    });
+});
+function removeActiveClasses() {
+    items.forEach(function (item){
+        item.classList.remove("active");
+    });
+};
+
+
+
+
+
+
